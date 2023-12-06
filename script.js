10d56d1c75aac274e2f044740fdf0a4275aad629
lufherna.V2 <lufherna.v2@gmail.com>
2023-12-05 23:06:37 -0500

    Added valid performance test with the POM design for the homepage

---
+++ script.js
@@ -0,0 +1,28 @@
+import { browser } from 'k6/experimental/browser';
+import { Homepage } from './page-objects/homepage.js';
+
+
+export const options = {
+  scenarios: {
+    shared_iter_scenario: {
+      executor: "shared-iterations",
+      vus: 10,
+      iterations: 100,
+      startTime: "0s",
+      options: {
+        browser: {
+          type: 'chromium'
+        }
+      }
+    }
+  }
+};
+
+
+export default async function() {
+  const page = browser.newPage();
+  const homePage = new Homepage(page);
+
+  await homePage.loginToHomePage()
+ 
+}
